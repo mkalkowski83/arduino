ec61da944e0d8b246dd7bcb226420b11bfba3df9
mkalkowski <michal.kalkowski@sumup.com>
2025-04-18 23:00:01 +0200

    change RgbLight to RgbLed

--- rgb/rgb.ino
+++ rgb/rgb.ino
@@ -1,12 +1,11 @@
-#include "RgbLight.h"
+#include "RgbLed.h"
 #include "Photoresistor.h"
-#include "TempSensor.h"
 
 Photoresistor photoresistor(A0);
+RgbLed rgbLed(9, 10, 11); // Assuming pins 9, 10, 11 for red, green, blue
 
 void setup() {
   Serial.begin(9600);
-
 }
 
 void loop() {
@@ -14,32 +13,27 @@ void loop() {
 
   // Turn on blue if light value is between 30 and 50
   if (lightValue >= 55) {
-    rgbLight.colorVariation(30, 30, 10);
+    rgbLed.colorVariation(30, 30, 10);
     delay(1000);
-    rgbLight.allOff();
+    rgbLed.allOff();
   }
 
   // Turn on red if light value is more than 50
   if (lightValue > 25 && lightValue < 55) {
-    rgbLight.colorVariation(127, 127, 50);
+    rgbLed.colorVariation(127, 127, 50);
     delay(1000);
-    rgbLight.allOff();
+    rgbLed.allOff();
   }
 
   // Turn on red if light value is more than 50
   if (lightValue < 25) {
-    rgbLight.colorVariation(255, 255, 255);
+    rgbLed.colorVariation(255, 255, 255);
     delay(1000);
-    rgbLight.allOff();
+    rgbLed.allOff();
   }
 
   Serial.print("Light level: ");
   Serial.print(lightValue);
   Serial.print("%");
-  Serial.println();
-
-  Serial.print("Current temperature: ");
-  Serial.print(tempSensor.readTemperature());
-  Serial.print("C");
   Serial.println();
 }
